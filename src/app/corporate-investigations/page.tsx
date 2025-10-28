3752c34707908713c5e0abc4d2b3a2aa3651cc8b
vicky <vickyempire723@gmail.com>
2025-10-28 10:21:11 +0530

    adding corporate-investigations page

---
+++ src/app/corporate-investigations/page.tsx
@@ -0,0 +1,11 @@
+import CorporateInvestigations from "@/components/corporate-investigations/corporate-investigations";
+
+
+
+export default function CorporateInvestigationsPage() {
+    return (
+        <>
+            <CorporateInvestigations />
+        </>
+    );
+}
