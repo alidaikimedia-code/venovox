78ea79ebcd7e7c78f16309b08fc2e7fb60df2192
BOLTTxx <153160622+BOLTTxx@users.noreply.github.com>
2025-11-04 16:24:51 +0800

    Patches
    Fixes on the submission form, import paths case sensitive fix.

--- public/career-handler.php
+++ public/career-handler.php
@@ -4,8 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit('Only POST requests allowed.');
 }
 
-$to = "kelly@venovox.com, dato.devan@venovox.com"; // Main recipients
-$bcc = "troubleshoot@venovox.com"; // BCC recipient
+$to = "kelly@venovox.com, dato.devan@venovox.com";
 $subject = 'VENOVOX: Career with Venovox';
 
 $firstName = htmlspecialchars($_POST['firstName'] ?? '');
@@ -48,8 +47,6 @@ if (isset($_FILES['cv']) && $_FILES['cv']['error'] === UPLOAD_ERR_OK) {
 
     // Headers for multipart email
     $headers = "From: Venovox Careers <no-reply@venovox.com>\r\n";
-    $headers .= "Reply-To: {$email}\r\n"; // Reply goes to applicant
-    $headers .= "Bcc: $bcc\r\n"; // BCC header
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
 
@@ -70,8 +67,6 @@ if (isset($_FILES['cv']) && $_FILES['cv']['error'] === UPLOAD_ERR_OK) {
 } else {
     // Simple HTML-only email
     $headers = "From: Venovox Careers <no-reply@venovox.com>\r\n";
-    $headers .= "Reply-To: {$email}\r\n"; // Reply goes to applicant
-    $headers .= "Bcc: $bcc\r\n"; // BCC header
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
